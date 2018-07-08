acc89493ba402c7b47fcca30066b3ce22041b707
mark.faiers <mark.faiers@spdspark.com>
2018-07-08 15:35:44 +0100

    Add further tests with different commands and number of command elements

--- PlutoRover.Tests/Controllers/PlutoRoverControllerTest.cs
+++ PlutoRover.Tests/Controllers/PlutoRoverControllerTest.cs
@@ -31,5 +31,124 @@ namespace PlutoRover.Tests.Controllers
             var expectedPosition = new Tuple<int, int, char>(0, 1, 'N');
             Assert.AreEqual(expectedPosition, rover.CurrentLocation);
         }
+
+        [TestMethod]
+        public void MoveOneSpaceBackwards_LocatioAfterMoveIs0Minus1N()
+        {
+            //Arrange
+
+            var rover = new Models.PlutoRover();
+
+            var roverController = new PlutoRoverController(rover);
+
+            var moveCommands = new[] { 'B' };
+
+            //Act
+
+            roverController.MoveRover(moveCommands);
+
+            //Assert
+
+            var expectedPosition = new Tuple<int, int, char>(0, -1, 'N');
+            Assert.AreEqual(expectedPosition, rover.CurrentLocation);
+
+        }
+
+        [TestMethod]
+        public void MoveTwoStepsForwards_LocationAfterMoveIs12N()
+        {
+            //Arrange
+            var rover = new Models.PlutoRover();
+
+            var roverController = new PlutoRoverController(rover);
+
+            var moveCommands = new[] { 'F', 'F' };
+
+            //Act
+            roverController.MoveRover(moveCommands);
+
+            //Assert
+            var expectedPosition = new Tuple<int, int, char>(0, 2, 'N');
+            Assert.AreEqual(expectedPosition, rover.CurrentLocation);
+        }
+
+        [TestMethod]
+        public void MoveOneSpaceForwardTurnRight_LocationAfterMoveIs01E()
+        {
+            //Arrange
+
+            var rover = new Models.PlutoRover();
+
+            var roverController = new PlutoRoverController(rover);
+
+            var moveCommands = new[] { 'F', 'R' };
+
+            //Act
+            roverController.MoveRover(moveCommands);
+
+            //Assert
+            var expectedPosition = new Tuple<int, int, char>(0, 1, 'E');
+            Assert.AreEqual(expectedPosition, rover.CurrentLocation);
+        }
+
+        [TestMethod]
+        public void TurnRightMoveOneSpaceForward_LocationAfterMoveIs10E()
+        {
+            //Arrange
+
+            var rover = new Models.PlutoRover();
+
+            var roverController = new PlutoRoverController(rover);
+
+            var moveCommands = new[] { 'R', 'F' };
+
+            //Act
+            roverController.MoveRover(moveCommands);
+
+            //Assert
+            var expectedPosition = new Tuple<int, int, char>(1, 0, 'E');
+            Assert.AreEqual(expectedPosition, rover.CurrentLocation);
+
+        }
+
+        [TestMethod]
+        public void MoveOneSpaceForwardTurnLeftMoveOnceSpaceForward_LoationAfterMoveIs1Minus1W()
+        {
+            //Arrange
+            var rover = new Models.PlutoRover();
+
+            var roverController = new PlutoRoverController(rover);
+
+            var moveCommands = new[] { 'F', 'L', 'F' };
+
+            //Act
+            roverController.MoveRover(moveCommands);
+
+            //Assert
+            var expectedPosition = new Tuple<int, int, char>(1, -1, 'W');
+            Assert.AreEqual(expectedPosition, rover.CurrentLocation);
+        }
+
+        [TestMethod]
+        public void MultipleMovesAndMultipleTurns_LocationAfterMoveIs0Minus1N()
+        {
+            //Arrange
+            var rover = new Models.PlutoRover();
+
+            var roverController = new PlutoRoverController(rover);
+
+            var moveCommands = new[] { 'B', 'L', 'F', 'R', 'F' };
+
+            //Act
+            roverController.MoveRover(moveCommands);
+
+            //Assert
+            var expectedPosition = new Tuple<int, int, char>(1, -1, 'W');
+            Assert.AreEqual(expectedPosition, rover.CurrentLocation);
+        }
+
+
+
+
     }
 }
